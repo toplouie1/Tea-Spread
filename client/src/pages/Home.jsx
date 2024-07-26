4294eb598a1619d7e4e09a0ab03ac3d25ae25d34
Nima sherpa <Toplouie78@gmail.com>
2024-07-26 13:22:30 -0400

    some fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../css/Home.css";
 
 function Home() {
+	const [checking, setChecking] = useState({});
+	useEffect(() => {
+		fetch("http://localhost:8000/users")
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => setChecking(data))
+			.catch((err) => console.log(err));
+	}, []);
+
+	const users = ["userOne", "userTwo", "userThree"];
+	console.log(checking.users);
+
 	return (
 		<div className="Home">
 			<div className="Hero">
@@ -23,13 +36,20 @@ function Home() {
 						alt="homeimg"
 						src="https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsb2ZmaWNlNV8zZF9yZW5kZXJfY2hhcmFjdGVyX29mX2FfeW91bmdfYXNpYW5fdGVhY2hlcl9zdF84OTk0MWU2NC1lNDM3LTQ0MTEtOWEyNi01MjcxZDlhMzUwMzNfMS5qcGc.jpg"
 					/>
-					<p>
+					{/* <p>
 						Welcome to TeaSpread, the all-in-one platform designed to streamline
 						and enhance the teaching experience. TeaSpread is your virtual
 						classroom assistant, allowing you to effortlessly assign homework,
 						manage classwork, create quizzes, leave messages for students, and
 						grade their work—all in one convenient place.
-					</p>
+					</p> */}
+					{checking.users && (
+						<p>
+							{checking.users.map((user) => (
+								<p key={user}>{user}</p>
+							))}
+						</p>
+					)}
 				</div>
 			</div>
 		</div>
--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
-app.get("/api", (req, res) => {
-	res.json({ users: ["userOne", "userTwo"] });
+// Middleware
+app.use(cors());
+// app.use(express.json());
+
+const PORT = 8000;
+
+app.get("/users", (req, res) => {
+	res.json({ users: ["userOne", "userTwo", "userThree"] });
+});
+app.get("/", (req, res) => {
+	res.send("Welcome To Tea Spread");
 });
 
-app.listen(8888, () => {
-	console.log("server started on 8888");
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
 });

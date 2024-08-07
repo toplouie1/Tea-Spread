b6fcb04cefeaa5d7361c5c3af31e302990ecc0d6
Nima sherpa <Toplouie78@gmail.com>
2024-08-07 15:25:57 -0400

    adding a Form / loading indicator

---
+++ client/src/components/LoadingIndicator.jsx
@@ -0,0 +1,11 @@
+import "../css/LoadingIndicator.css";
+
+const LoadingIndicator = () => {
+	return (
+		<div className="loading-container">
+			<div className="loader"></div>
+		</div>
+	);
+};
+
+export default LoadingIndicator;

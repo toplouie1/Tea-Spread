7267d9ce7bbe347a6e84145129957885df01b103
Nima sherpa <Toplouie78@gmail.com>
2024-08-29 14:57:29 -0400

    nav update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,21 +16,6 @@ function Home() {
 					</h3>
 				</div>
 			</div>
-			<div className="introduction">
-				<div className="intro-container">
-					<img
-						alt="homeimg"
-						src="https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsb2ZmaWNlNV8zZF9yZW5kZXJfY2hhcmFjdGVyX29mX2FfeW91bmdfYXNpYW5fdGVhY2hlcl9zdF84OTk0MWU2NC1lNDM3LTQ0MTEtOWEyNi01MjcxZDlhMzUwMzNfMS5qcGc.jpg"
-					/>
-					<p>
-						Welcome to TeaSpread, the all-in-one platform designed to streamline
-						and enhance the teaching experience. TeaSpread is your virtual
-						classroom assistant, allowing you to effortlessly assign homework,
-						manage classwork, create quizzes, leave messages for students, and
-						grade their work—all in one convenient place.
-					</p>
-				</div>
-			</div>
 		</div>
 	);
 }

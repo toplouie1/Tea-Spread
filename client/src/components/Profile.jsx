600738047657776b7f04c1ab0949d6ad845f20f6
Nima sherpa <Toplouie78@gmail.com>
2024-09-06 16:36:14 -0400

    create profiles / fix

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "../css/Profile.css";
+
+const API = import.meta.env.VITE_API_URL;
 
 const Profile = () => {
 	const [formData, setFormData] = useState({
+		user_id: Number(localStorage.getItem("userId")) || 0,
 		email: "",
 		first_name: "",
 		last_name: "",
@@ -19,22 +23,24 @@ const Profile = () => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log("Form submitted:", formData);
+		axios
+			.post(`${API}/profiles`, formData, {
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
+			.then((res) => {
+				navigate("/");
+			})
+			.catch((error) => {
+				if (error.response && error.response.data) {
+					console.log(error);
+				}
+			});
 	};
 
 	return (
 		<form className="profile_form" onSubmit={handleSubmit}>
-			<div>
-				<label>Email:</label>
-				<input
-					className="profile_input"
-					type="email"
-					name="email"
-					value={formData.email}
-					onChange={handleChange}
-					required
-				/>
-			</div>
 			<div>
 				<label className="profile_label">First Name:</label>
 				<input
@@ -53,6 +59,17 @@ const Profile = () => {
 					type="text"
 					name="last_name"
 					value={formData.last_name}
+					onChange={handleChange}
+					required
+				/>
+			</div>
+			<div>
+				<label>Email:</label>
+				<input
+					className="profile_input"
+					type="email"
+					name="email"
+					value={formData.email}
 					onChange={handleChange}
 					required
 				/>

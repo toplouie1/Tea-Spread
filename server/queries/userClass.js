f597121dd72607f7a42fdd7f84379e1b79e85865
Nima sherpa <Toplouie78@gmail.com>
2024-09-21 09:59:22 -0400

    remove try on users query

--- server/queries/userClass.js
+++ server/queries/userClass.js
@@ -1,16 +1,11 @@
 const db = require("../db/dbConfig.js");
 
 const getUserClasses = async (userId) => {
-	try {
-		const classes = await db.many(
-			"SELECT class_id FROM class_participants WHERE user_id = $1",
-			[userId]
-		);
-		return classes;
-	} catch (error) {
-		console.error("Error fetching classes for the user:", error);
-		throw new Error("Unable to fetch classes.");
-	}
+	const classes = await db.many(
+		"SELECT class_id FROM class_participants WHERE user_id = $1",
+		[userId]
+	);
+	return classes;
 };
 
 module.exports = {
--- server/queries/users.js
+++ server/queries/users.js
@@ -2,54 +2,32 @@ const db = require("../db/dbConfig.js");
 const bcrypt = require("bcrypt");
 
 const getAllUsers = async () => {
-	try {
-		var users = await db.many("SELECT * FROM users");
-		return users;
-	} catch (error) {
-		return error;
-	}
+	const users = await db.many("SELECT * FROM users");
+	return users;
 };
 
 const getOneUser = async (uid) => {
-	try {
-		const user = await db.one("SELECT * FROM users WHERE user_id=$1", uid);
-		return user;
-	} catch (error) {
-		return error;
-	}
+	const user = await db.one("SELECT * FROM users WHERE user_id=$1", uid);
+	return user;
 };
 
 const createUser = async (user) => {
-	try {
-		const createdUser = await db.one(
-			"INSERT INTO users(username, password_hash) VALUES($1,$2) RETURNING *",
-			[user.username, user.password_hash]
-		);
-		return createdUser;
-	} catch (error) {
-		return error;
-	}
+	const createdUser = await db.one(
+		"INSERT INTO users(username, password_hash) VALUES($1,$2) RETURNING *",
+		[user.username, user.password_hash]
+	);
+	return createdUser;
 };
 
 const authUser = async (username, password) => {
-	try {
-		const user = await db.one(
-			"SELECT * FROM users WHERE username=$1",
-			username
-		);
-
-		const match = await bcrypt.compare(password, user.password_hash);
-		if (match) {
-			const userInfo = {
-				user_id: user.user_id,
-				username: user.username,
-			};
-			return userInfo;
-		} else {
-			return { error: "Incorrect password" };
-		}
-	} catch (e) {
-		return { error: "Username doesn't exist..." };
+	const user = await db.one("SELECT * FROM users WHERE username=$1", username);
+	const match = await bcrypt.compare(password, user.password_hash);
+	if (match) {
+		const userInfo = {
+			user_id: user.user_id,
+			username: user.username,
+		};
+		return userInfo;
 	}
 };
 
